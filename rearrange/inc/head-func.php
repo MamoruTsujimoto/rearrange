271cbadb94a0e43457899ba8b594bb563dd31bd0
MamoruTsujimoto <tsujimotosirchristophermamoru@gmail.com>
2020-05-20 20:42:08 +0900

    fix

--- rearrange/inc/head-func.php
+++ rearrange/inc/head-func.php
@@ -226,7 +226,7 @@ add_action( 'wp_head', function() {
     echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimal-ui" />';
     echo '<meta name="mobile-web-app-capable" content="yes" />';
     echo '<meta name="apple-mobile-web-app-capable" content="yes" />';
-    echo '<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />';
+    echo '<meta name="apple-mobile-web-app-status-bar-style" content="black" />';
 
     // Description
     rearrange_the_description_tag();
