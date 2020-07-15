48ffb79ec0aea713107788837e19700e7e1c366a
MamoruTsujimoto <tsujimotosirchristophermamoru@gmail.com>
2020-07-15 16:08:04 +0900

    fix: lizard position

--- rearrange/components/global-header.php
+++ rearrange/components/global-header.php
@@ -3,7 +3,8 @@ global $rearrange;
 ?>
 <header id="global-header">
   <div id="wellcome-to">
-    <h1><a href="<?php echo $rearrange['home_url']; ?>" title="<?php echo $rearrange['site_name']; ?>"><?php echo $rearrange['site_name']; ?></a><img src="<?php echo get_theme_file_uri(); ?>/assets/img/lizard.png" class="lizard"></h1>
+    <h1><a href="<?php echo $rearrange['home_url']; ?>" title="<?php echo $rearrange['site_name']; ?>"><?php echo $rearrange['site_name']; ?></a></h1>
+    <img src="<?php echo get_theme_file_uri(); ?>/assets/img/lizard.png" class="lizard">
     <div id="menu"><a class="menu-trigger" href="#"><span></span></a></div>
   </div>
 </header>
